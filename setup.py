d57522600d5a982466e61c5c78afbc8530220fe2
Raul Rodrigo Segura <raurodse@gmail.com>
2017-11-14 08:49:34 +0100

    add structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+	setup(name='edupals-base',
+		version='0.1',
+		description='',
+		long_description="""""",
+		author='Raul Rodrigo Segura',
+		author_email='raurodse@gmail.com',
+		maintainer='Raul Rodrigo Segura',
+		maintainer_email='raurodse@gmail.com',
+		keywords=['software',''],
+		url='https://github.com/edupals/python-edupals-base',
+		license='GPL',
+		platforms='UNIX',
+		packages = find_packages()
+	)
