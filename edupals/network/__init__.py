8155eba21e748044ed269fbce82843e10bb1ab68
Lt-Henry <quiqueiii@gmail.com>
2022-07-26 01:06:27 +0200

    * implemented get mtu and type from interface * implemented ipv4 range check using ipaddress lib only

--- edupals/network/__init__.py
+++ edupals/network/__init__.py
@@ -3,6 +3,9 @@ import ipaddress
 import socket
 import os
 
+SYS_TYPE_ETHERNET = 1
+SYS_TYPE_LOOPBACK = 772
+
 class MacAddress:
     address = [0x00,0x00,0x00,0x00,0x00,0x00]
 
@@ -84,18 +87,30 @@ class Interface:
         else:
             return []
 
-    def carrier(self):
-        try:
-            f=open(self.device_path+"/carrier","rt")
-            line = f.readline().strip()
-            f.close()
+    def _read_sys(self,node):
+        f=open(self.device_path+"/"+node,"rt")
+        line = f.readline().strip()
+        f.close()
 
+        return line
+
+    def get_carrier(self):
+        try:
+            line = self._read_sys("carrier")
             if(line=="1"):
                 return True
             else:
                 return False
         except:
             return False
+
+    def get_type(self):
+        line = self._read_sys("type")
+        return int(line)
+
+    def get_mtu(self):
+        line = self._read_sys("mtu")
+        return int(line)
 
 def get_ip_from_host(host):
     '''
@@ -105,3 +120,17 @@ def get_ip_from_host(host):
         return ipaddress.ip_address(socket.gethostbyname(host))
     except Exception:
         return None
+
+def is_ip_in_range(ip,ip_network):
+    '''
+    Checks whether an ip is a child of a certain network.
+    Returns True or False.
+    ex:
+        is_ip_in_range("10.0.0.160","10.0.0.128/25")
+    '''
+    #ip_network ex: "10.0.0.128/24"
+    try:
+        ip=ipaddress.IPv4Address(ip)
+        return ip in list(ipaddress.ip_network(ip_network).hosts())
+    except Exception:
+        return False
