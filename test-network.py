4366f884198ee7abf91addb2e6a500df0f0a6275
Lt-Henry <quiqueiii@gmail.com>
2025-09-22 11:13:05 +0200

    * virtual interface check * wireless interface check * gets default route interface

--- test-network.py
+++ test-network.py
@@ -7,6 +7,9 @@ ifaces = network.Interface.interfaces()
 for iface in ifaces:
     print()
     print(iface.name)
+    print("virtual:{0}".format(iface.is_virtual()))
+    print("wireless:{0}".format(iface.is_wireless()))
+
     iface_type = iface.get_type()
     print("type:",iface_type)
     if (iface_type==network.SYS_TYPE_ETHERNET):
@@ -17,8 +20,7 @@ for iface in ifaces:
         print("* ",addr)
         #print("broadcast:",addr.is_broadcast())
 
-
 #print(network.get_ip_from_host("rapture"))
 
 print(network.is_ip_in_range("192.168.1.2","192.168.1.0/24"))
-
+print("default interface:{0}".format(network.get_default_interface()))
